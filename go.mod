e54f4d6489995f7fdc34ff26b46b5373a08b886f
rkoster <hi@rkoster.dev>
2021-11-24 21:06:00 +0100

    Added progress logging

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/rkoster/github-multi-repo-project-card-sync
 go 1.16
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/shurcooL/githubv4 v0.0.0-20210922025249-6831e00d857f
 	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
 
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -11,9 +12,11 @@ import (
 )
 
 func main() {
+	logger := log.Default()
+
 	c, err := config.LoadConfig("config.yml")
 	if err != nil {
-		panic(err)
+		logger.Fatalf("failed to load config: %s", err)
 	}
 
 	token := os.Getenv("GITHUB_TOKEN")
@@ -22,144 +25,147 @@ func main() {
 
 	project, err := gh.GetOrganizationProject(c.Project.Organization, c.Project.Number, ctx)
 	if err != nil {
-		panic(err)
+		logger.Fatalf("failed to load github project: %s", err)
 	}
 
-	for _, repo := range c.Repositories {
+	for i, repo := range c.Repositories {
 		pullRequests, err := gh.ListOpenPullRequests(repo.Name, ctx)
 		if err != nil {
-			panic(err)
-		}
-		err = processPullRequests(pullRequests, project, repo, gh, ctx)
-		if err != nil {
-			panic(err)
+			logger.Fatalf("failed to list pull requests for: %s got: %s", repo.Name, err)
 		}
 		issues, err := gh.ListOpenIssues(repo.Name, ctx)
 		if err != nil {
-			panic(err)
+			logger.Fatalf("failed to list issues for: %s got: %s", repo.Name, err)
 		}
-		err = processIssues(issues, project, repo, gh, ctx)
-		if err != nil {
-			panic(err)
+		fmt.Printf("\n[%d/%d] %s processing: %d ", i, len(c.Repositories), repo.Name, len(pullRequests)+len(issues))
+		for _, pullRequest := range pullRequests {
+			err = processPullRequest(pullRequest, project, repo, gh, ctx)
+			if err != nil {
+				logger.Fatalf("failed to process pull request: %s got: %s", pullRequest.URL, err)
+			}
+			fmt.Print(".")
+		}
+		for _, issue := range issues {
+			err = processIssue(issue, project, repo, gh, ctx)
+			if err != nil {
+				logger.Fatalf("failed to process issue: %s got: %s", issue.URL, err)
+			}
+			fmt.Print(".")
 		}
 	}
 }
 
-func processIssues(issues []github.Issue, project github.Project, repo config.Repository, gh *github.Client, ctx context.Context) error {
-	for _, issue := range issues {
-		item, err := gh.AddProjectItem(project.ID, issue.ID, ctx)
-		if err != nil {
-			return err
+func processIssue(issue github.Issue, project github.Project, repo config.Repository, gh *github.Client, ctx context.Context) error {
+	item, err := gh.AddProjectItem(project.ID, issue.ID, ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, field := range repo.Fields {
+		f, found := project.Fields.FindByName(field.Name)
+		if !found {
+			return fmt.Errorf("Project does not have field with name: %s", field.Name)
 		}
 
-		for _, field := range repo.Fields {
-			f, found := project.Fields.FindByName(field.Name)
-			if !found {
-				return fmt.Errorf("Project does not have field with name: %s", field.Name)
-			}
+		var value string
 
-			var value string
-
-			switch field.Type {
-			case "draft":
+		switch field.Type {
+		case "draft":
+			continue
+		case "author":
+			value = issue.Author.Login
+		case "last_activity":
+			value = issue.TimelineItems.UpdatedAt.String()
+		case "default_single_select":
+			currentValue, found := item.FieldValues.Nodes.FindByID(f.ID)
+			if found && currentValue.Value != "" {
 				continue
-			case "author":
-				value = issue.Author.Login
-			case "last_activity":
-				value = issue.TimelineItems.UpdatedAt.String()
-			case "default_single_select":
-				currentValue, found := item.FieldValues.Nodes.FindByID(f.ID)
-				if found && currentValue.Value != "" {
-					continue
-				}
-				o, found := f.FindOptionByName(field.Value)
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
-				}
-				value = o.ID
-			case "single_select":
-				o, found := f.FindOptionByName(field.Value)
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
-				}
-				value = o.ID
-			case "type":
-				o, found := f.FindOptionByName("Issue")
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, "Issue")
-				}
-				value = o.ID
-			default:
-				return fmt.Errorf(
-					"Only 'draft', 'author' and 'single_select' are currently supported values for field.type, given: %v", field.Type)
 			}
+			o, found := f.FindOptionByName(field.Value)
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
+			}
+			value = o.ID
+		case "single_select":
+			o, found := f.FindOptionByName(field.Value)
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
+			}
+			value = o.ID
+		case "type":
+			o, found := f.FindOptionByName("Issue")
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, "Issue")
+			}
+			value = o.ID
+		default:
+			return fmt.Errorf(
+				"Only 'draft', 'author' and 'single_select' are currently supported values for field.type, given: %v", field.Type)
+		}
 
-			_, err := gh.UpdateProjectItemField(project.ID, item.ID, f.ID, value, ctx)
-			if err != nil {
-				return err
-			}
+		_, err := gh.UpdateProjectItemField(project.ID, item.ID, f.ID, value, ctx)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func processPullRequests(pullRequests []github.PullRequest, project github.Project, repo config.Repository, gh *github.Client, ctx context.Context) error {
-	for _, pullRequest := range pullRequests {
-		item, err := gh.AddProjectItem(project.ID, pullRequest.ID, ctx)
-		if err != nil {
-			return err
+func processPullRequest(pullRequest github.PullRequest, project github.Project, repo config.Repository, gh *github.Client, ctx context.Context) error {
+	item, err := gh.AddProjectItem(project.ID, pullRequest.ID, ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, field := range repo.Fields {
+		f, found := project.Fields.FindByName(field.Name)
+		if !found {
+			return fmt.Errorf("Project does not have field with name: %s", field.Name)
 		}
 
-		for _, field := range repo.Fields {
-			f, found := project.Fields.FindByName(field.Name)
+		var value string
+
+		switch field.Type {
+		case "draft":
+			o, found := f.FindOptionByName(strconv.FormatBool(pullRequest.IsDraft))
 			if !found {
-				return fmt.Errorf("Project does not have field with name: %s", field.Name)
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
 			}
-
-			var value string
-
-			switch field.Type {
-			case "draft":
-				o, found := f.FindOptionByName(strconv.FormatBool(pullRequest.IsDraft))
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
-				}
-				value = o.ID
-			case "author":
-				value = pullRequest.Author.Login
-			case "last_activity":
-				value = pullRequest.TimelineItems.UpdatedAt.String()
-			case "default_single_select":
-				currentValue, found := item.FieldValues.Nodes.FindByID(f.ID)
-				if found && currentValue.Value != "" {
-					continue
-				}
-				o, found := f.FindOptionByName(field.Value)
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
-				}
-				value = o.ID
-			case "single_select":
-				o, found := f.FindOptionByName(field.Value)
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
-				}
-				value = o.ID
-			case "type":
-				o, found := f.FindOptionByName("Pull Request")
-				if !found {
-					return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, "Issue")
-				}
-				value = o.ID
-			default:
-				return fmt.Errorf(
-					"Only 'draft', 'author' and 'single_select' are currently supported values for field.type, given: %v", field.Type)
+			value = o.ID
+		case "author":
+			value = pullRequest.Author.Login
+		case "last_activity":
+			value = pullRequest.TimelineItems.UpdatedAt.String()
+		case "default_single_select":
+			currentValue, found := item.FieldValues.Nodes.FindByID(f.ID)
+			if found && currentValue.Value != "" {
+				continue
 			}
-
-			_, err := gh.UpdateProjectItemField(project.ID, item.ID, f.ID, value, ctx)
-			if err != nil {
-				return err
+			o, found := f.FindOptionByName(field.Value)
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
 			}
+			value = o.ID
+		case "single_select":
+			o, found := f.FindOptionByName(field.Value)
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, field.Value)
+			}
+			value = o.ID
+		case "type":
+			o, found := f.FindOptionByName("Pull Request")
+			if !found {
+				return fmt.Errorf("Project field: %s does not have an option: %s", field.Name, "Issue")
+			}
+			value = o.ID
+		default:
+			return fmt.Errorf(
+				"Only 'draft', 'author' and 'single_select' are currently supported values for field.type, given: %v", field.Type)
+		}
+
+		_, err := gh.UpdateProjectItemField(project.ID, item.ID, f.ID, value, ctx)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
